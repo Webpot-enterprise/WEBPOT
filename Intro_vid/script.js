58b79b07fbe97f2fbb1765b0bbd6119505290eef
Webpot-enterprise <engagewebpot@gmail.com>
2026-02-20 21:05:19 +0530

    loader linked

--- Intro_vid/script.js
+++ Intro_vid/script.js
@@ -1,1 +1,16 @@
-// Reserved for intro loader interactions.
+const LOAD_DURATION_MS = 4000;
+const EXIT_FADE_MS = 300;
+
+window.addEventListener("DOMContentLoaded", () => {
+  const loaderScreen = document.getElementById("loaderScreen");
+
+  setTimeout(() => {
+    if (loaderScreen) {
+      loaderScreen.classList.add("is-exiting");
+    }
+
+    setTimeout(() => {
+      window.location.href = "../index.html";
+    }, EXIT_FADE_MS);
+  }, LOAD_DURATION_MS);
+});
